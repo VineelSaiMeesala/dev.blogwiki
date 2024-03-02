740a22e0aae868f1d595a388620b5fe6193e0e99
Meesala Vineel Sai <85602650+VineelSaiMeesala@users.noreply.github.com>
2024-03-02 15:25:07 +0530

    Delete all files

--- src/Components/ErrorHandle.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Error from "./Error";
-import homepage from "../Pages/HomePage";
-const Errorhandle = () => {
-  Error(homepage, "hello");
-};
-export default Errorhandle;

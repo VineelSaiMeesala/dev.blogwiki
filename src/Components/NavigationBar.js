0deb8e5473d2540e0064351dad2a60c7f5d78541
Meesala Vineel Sai <85602650+VineelSaiMeesala@users.noreply.github.com>
2024-03-23 11:12:55 +0530

    BW-17 Initail design deplyed to dev without data

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -45,7 +45,7 @@ var NavigationBar = function () {
             <Link to="/BlogPage">Services</Link>
           </span>
           <span>
-            <Link to="/testpages">TestLink</Link>
+            <Link to="/testpage">TestLink</Link>
           </span>
         </div>
         <div className="search">
